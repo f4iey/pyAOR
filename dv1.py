f40e07ddfd15f4d45d5801f3da330ac57763a809
Jules f4iey <93203507+f4iey@users.noreply.github.com>
2023-03-25 12:11:25 +0100

    remove tts

--- dv1.py
+++ dv1.py
@@ -1,7 +1,5 @@
 # List of functions to control AOR AR-DV1
 import serial
-import pyttsx3
-
 class Command:
     def __init__(self, name, parameter, desc):
         self.name = name
@@ -13,16 +11,14 @@ class Command:
     def send(self, ser):
         ser.write((self.name + self.param + "\r").encode('ascii', 'ignore'))
 
-    def response(self, ser, engine):
+    def response(self, ser):
         res = ser.readline().decode()
         for k in self.result_code.keys():
             if res[0:2] == k and k != "20":
                 print(self.result_code[k])
                 return
         if len(res) <= 5:
-            engine.say(self.desc + " : " + self.param)
-            engine.runAndWait()
+            print(self.desc + " : " + self.param)
             return
-        engine.say(self.desc + " : " + res[4:])
-        engine.runAndWait()
+        print(self.desc + " : " + res[4:])
         return res [4:]
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import serial
-import pyttsx3
 from Commands import *
-
-# Initialisation de la synthèse vocale
-engine = pyttsx3.init()
 
 # Ouverture du port série
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
@@ -11,7 +7,7 @@ ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 # Première commande pour afficher la fréquence
 Commands = Commands()
 rf.send(ser)
-print(rf.response(ser, engine))
+rf.response(ser)
 
 # Fermeture du port série
 ser.close()
