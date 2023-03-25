1d5bb038e9f1ce9c4f67019d24a3857d2f00110a
f4iey <benkemounjules@gmail.com>
2023-03-25 19:55:20 +0100

    change example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import serial
+import time
 import pyttsx3
 from Commands import *
 
@@ -9,8 +10,17 @@ engine.setProperty('voice', 'fr')
 # Ouverture du port série
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 
-# Première commande pour afficher la fréquence
+# Première commande d'exemple qui reboot et prononce la freq
 Commands = Commands()
+Commands.qp.send(ser)
+print(Commands.qp.response(ser, engine))
+time.sleep(3)
+Commands.zp.send(ser)
+print(Commands.zp.response(ser, engine))
+time.sleep(4)
+ser.close()
+# On rouvre le session pour lire la freq de nouveau
+ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 Commands.rf.send(ser)
 print(Commands.rf.response(ser, engine))
 
