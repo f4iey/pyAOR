2ae5c5ceee64edfee29b4d1276d63a57e223a6af
f4iey <benkemounjules@gmail.com>
2023-03-25 19:19:58 +0100

    add french voice

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import serial
 import pyttsx3
 from Commands import *
 
-# Initialisation de la synthèse vocale
+# Initialisation de la synthèse vocale en francais
 engine = pyttsx3.init()
+engine.setProperty('voice', 'fr')
 
 # Ouverture du port série
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 
 # Première commande pour afficher la fréquence
 Commands = Commands()
-rf.send(ser)
-print(rf.response(ser, engine))
+Commands.rf.send(ser)
+print(Commands.rf.response(ser, engine))
 
 # Fermeture du port série
 ser.close()
